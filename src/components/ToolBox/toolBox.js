d2dbf61730098fb24e32b8f0fbf34205a2631163
Kheoh Yee Wei <kwei88@gmail.com>
2019-01-14 21:48:16 +0800

    Toolbox dropdown

--- src/components/ToolBox/toolBox.js
+++ src/components/ToolBox/toolBox.js
@@ -1,21 +1,55 @@
 import { html, render } from "lit-html";
 import "./toolbox.scss";
 
+function handleClickToolBox() {
+  this.classList.toggle("active");
+}
+
+// function handleKeydown(e) {
+//   if (e.keyCode === 40) {
+//     handleClickToolBox.call(this);
+//   }
+// }
+
 export default function toolBoxElm() {
   return html`
-    <div class="toolBox">
-      <div class="toolBox-icon-and-desc">
+    <div>
+      <button
+        @click=${handleClickToolBox}
+        aria-expanded="false"
+        aria-haspopup="true"
+        class="btn-toolBox"
+      >
+        <div class="flex-col-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            viewBox="0 0 24 24"
+            class="toolBox-add-icon"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"
+            />
+          </svg>
+          <h2 id="aria-toolBox">Add a form control</h2>
+        </div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="50"
-          height="50"
+          width="30"
+          height="30"
           viewBox="0 0 24 24"
-          class="toolBox-add-icon"
+          class="toolBox-arrow-dropdown-icon"
         >
-          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+          <path d="M7 10l5 5 5-5z" />
           <path d="M0 0h24v24H0z" fill="none" />
         </svg>
-        <p>Add a form control</p>
+      </button>
+      <div class="toolBox-menu" role="menu">
+        <button class="toolBox-menu-item" role="menuitem">Text</button>
+        <button class="toolBox-menu-item" role="menuitem">Checkbox</button>
+        <button class="toolBox-menu-item" role="menuitem">Radio Button</button>
       </div>
     </div>
   `;
