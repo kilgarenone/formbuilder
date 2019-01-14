1e27d2dfe36c09fc1a5ce87fd4082216db9ed9f5
Kheoh Yee Wei <kwei88@gmail.com>
2019-01-14 17:23:42 +0800

    Input title of form

--- src/components/Builder/builder.js
+++ src/components/Builder/builder.js
@@ -1,1 +1,28 @@
 import "./builder.scss";
+import { html, render } from "lit-html";
+
+const formColTitlePlaceholder = html`
+  <h1 class="form-col-title-placeholder">Title</h1>
+`;
+
+const formColTitleElm = (doNotShowPlaceholder = false) => html`
+  <div class="form-col">
+    <div class="form-col-title">
+      <h1
+        @input=${keyup}
+        class="form-col-title-value"
+        contenteditable="true"
+        spellcheck="false"
+      ></h1>
+      ${!doNotShowPlaceholder ? formColTitlePlaceholder : ""}
+    </div>
+  </div>
+`;
+
+const builderElm = document.getElementById("builder");
+
+function keyup(e) {
+  render(formColTitleElm(!!e.target.textContent.trim()), builderElm);
+}
+
+render(formColTitleElm(), builderElm);
