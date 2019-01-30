88236c07c4df21d1f0e938b36a05a83ef98e9ccc
Kheoh Yee Wei <kwei88@gmail.com>
2019-01-30 09:55:07 +0800

    Completes proof of concept for SSR for form template and css

--- src/lib/client.js
+++ src/lib/client.js
@@ -3,8 +3,22 @@ import { ClientInputMasking } from "./inputMasking";
 fetch("http://localhost:3006/hello")
   .then(response => response.json())
   .then(data => {
-    const root = document.getElementById("app");
-    document.head.innerHTML = document.head.innerHTML + data.css;
-    root.innerHTML = data.html;
     console.log("data", data);
+    const root = document.getElementById("app");
+    const temp = document.createElement("div");
+
+    temp.innerHTML = data.css;
+
+    const head = document.head;
+
+    // insert the styles tags
+    while (temp.firstChild) {
+      head.appendChild(temp.firstChild);
+    }
+
+    // insert the form templates
+    root.innerHTML = data.html;
+
+    // TODO: probably use loadCss/loadjs to execute
+    // codes when css/dom are loaded
   });
