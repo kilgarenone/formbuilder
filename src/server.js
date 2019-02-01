1f6043d3f3f072326199f1abc9823c5b8639dfeb
Kheoh Yee Wei <kwei88@gmail.com>
2019-02-01 18:36:51 +0800

    Executes auth code flow OAuth2.0

--- src/server.js
+++ src/server.js
@@ -9,12 +9,13 @@ import { flushToHTML } from "styled-jsx/server";
 import helmet from "helmet";
 import morgan from "morgan";
 import jwt from "express-jwt";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
 
 import ClientFormGenerator from "./components/ClientFormGenerator";
 
-const PORT = process.env.PORT || 3006;
 const server = express();
-
+console.log(process.env.PORT);
 server.use(helmet());
 server.use(bodyParser.json());
 server.use(cors());
@@ -27,15 +28,34 @@ const forms = [
 // tell Express to serve contents from the build directory as static files.
 server.use(express.static(path.resolve("public")));
 
-server.get("/hello", (req, res) => {
-  const markup = ReactDOM.renderToStaticMarkup(
-    <ClientFormGenerator forms={forms} />
-  );
+server.get("/callback", (req, res) => {
+  const body = {
+    grant_type: "authorization_code",
+    code: req.query.code,
+    client_id: process.env.AUTH0_CLIENT_ID,
+    client_secret: process.env.AUTH0_SECRET,
+    redirect_uri: process.env.AUTH0_REDIRECT_URI
+  };
 
-  const styles = flushToHTML();
+  fetch(`${process.env.AUTH0_API_BASEURL}/oauth/token`, {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(res => res.json())
+    .then(json => {
+      console.log("auto0 res", json);
+      res.redirect(process.env.AUTH0_REDIRECT_SPA);
+    });
+});
 
-  res.json({ css: styles, html: markup });
-
+server.get("/", (req, res) => {
+  // res.redirect("http://localhost:8080");
+  // const markup = ReactDOM.renderToStaticMarkup(
+  //   <ClientFormGenerator forms={forms} />
+  // );
+  // const styles = flushToHTML();
+  // res.json({ css: styles, html: markup });
   // const html = ReactDOM.renderToStaticMarkup(
   //   <html>
   //     <head>{styles}</head>
@@ -45,6 +65,8 @@ server.get("/hello", (req, res) => {
   //   </html>
   // );
 });
+
+const PORT = process.env.PORT || 3006;
 
 server.listen(PORT, () => {
   console.log(`😎  Server is listening on port ${PORT}`);
