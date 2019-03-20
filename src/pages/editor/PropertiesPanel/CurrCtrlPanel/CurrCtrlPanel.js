0b16e8ec168e54d04ba21f22513b828687e2572a
Kheoh Yee Wei <kwei88@gmail.com>
2019-03-20 15:10:42 +0800

    Currencies symbol dropdown

--- src/pages/editor/PropertiesPanel/CurrCtrlPanel/CurrCtrlPanel.js
+++ src/pages/editor/PropertiesPanel/CurrCtrlPanel/CurrCtrlPanel.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Label from "../../../../components/ControlLabel/ControlLabel";
+import Dropdown from "../../../../components/Dropdown/Dropdown";
+
+const CURRENCY = [
+  { type: "usd", desc: "\u0024 USD" },
+  { type: "eur", desc: "\u20AC EUR" },
+  { type: "gbp", desc: "\u00A3 GBP" },
+  { type: "rmb", desc: "\u00A5 RMB" },
+  { type: "inr", desc: "\u20B9 INR" }
+];
 
 class CurrCtrlPanel extends Component {
   state = {};
 
   render() {
-    return <div>Currency Pabel yall</div>;
+    return (
+      <div>
+        <Label>Currency</Label>
+        <Dropdown
+          items={CURRENCY}
+          handleSelectedItem={this.handleSelectedTheme}
+          initialActiveItemDesc="USD"
+        />
+      </div>
+    );
   }
 }
 
